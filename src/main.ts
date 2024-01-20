5ef5703adb9f19e4eaa5e418a9d18e4f55ed4daa
Will G <sniper7kills@users.noreply.github.com>
2024-01-20 13:18:01 -0500

    PrimeVue Tailwind Installed

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,16 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import PrimeVue from 'primevue/config';
+import Wind from '@/presets/wind';
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+    unstyled: true,
+    pt: Wind
+});
 
 app.mount('#app')
