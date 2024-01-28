dc92e950db9eb2dbb270135f287a45ca1697bda8
Will G <sniper7kills@users.noreply.github.com>
2024-01-27 21:19:08 -0500

    WIP

--- src/components/core/device/CoreDeviceSelect.vue
+++ src/components/core/device/CoreDeviceSelect.vue
@@ -3,7 +3,7 @@
     <span class="relative flex-grow">
       <label
         v-if="$attrs.label"
-        :for="$attrs.inputId as string | undefined"
+        :for="$attrs.inputId as string"
         class="text-sm text-primary-500 dark:text-primary-400/60 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4"
       >
         {{ $attrs.label }}
